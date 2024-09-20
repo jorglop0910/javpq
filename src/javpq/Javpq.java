f4b99df790508b1d05252e7bf15a94ffa7c8ff63
jorglop0910 <lopeztinocojorgealejandro@gmail.com>
2024-09-20 11:03:14 -0600

    Nuevo aviso

--- src/javpq/Javpq.java
+++ src/javpq/Javpq.java
@@ -16,6 +16,7 @@ public class Javpq {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola");
+        System.out.println("Nuevo aviso");
     }
     
 }
